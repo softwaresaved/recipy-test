8ae4e2a5ddc623d6c19a873159036a3d35056da1
Mike Jackson <michaelj@epcc.ed.ac.uk>
2016-08-19 01:17:54 -0700

    mkdir all take -p

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -4,21 +4,21 @@ MODULE=""
 mkdir -p tmp
 rm -rf tmp/*
 
-mkdir tmp/matplotlib
+mkdir -p tmp/matplotlib
 python $MODULE run_matplotlib.pyplot.py savefig tmp/matplotlib/plot.png
 
-mkdir tmp/bs4
+mkdir -p tmp/bs4
 python $MODULE run_bs4.py BeautifulSoup data/bs4/index.html
 
-mkdir tmp/lxml
+mkdir -p tmp/lxml
 python $MODULE run_lxml.etree.py parse data/lxml/data.xml
 python $MODULE run_lxml.etree.py iterparse data/lxml/data.xml
 
-mkdir tmp/PIL
+mkdir -p tmp/PIL
 python $MODULE run_PIL.py Image.open data/PIL/image.png
 python $MODULE run_PIL.py Image.save data/PIL/image.png tmp/PIL/rotated.png
 
-mkdir tmp/numpy
+mkdir -p tmp/numpy
 python $MODULE run_numpy.py loadtxt data/numpy/data.csv
 python $MODULE run_numpy.py fromfile data/numpy/data.csv
 python $MODULE run_numpy.py genfromtxt data/numpy/data_incomplete.csv
@@ -27,11 +27,11 @@ python $MODULE run_numpy.py savez tmp/numpy/data.npz
 python $MODULE run_numpy.py savez_compressed tmp/numpy/dataz.npz
 python $MODULE run_numpy.py savetxt tmp/numpy/data.txt
 
-mkdir tmp/sklearn
+mkdir -p tmp/sklearn
 python $MODULE run_sklearn.py dump_svmlight_file tmp/sklearn/data.svmlight
 python $MODULE run_sklearn.py load_svmlight_file data/sklearn/data.svmlight
 
-mkdir tmp/skimage
+mkdir -p tmp/skimage
 python $MODULE run_skimage.py io.imread data/skimage/image.png 
 python $MODULE run_skimage.py io.imsave data/skimage/image.png tmp/skimage/rotated.png
 python $MODULE run_skimage.py external.tifffile.imread data/skimage/image.tiff
@@ -39,7 +39,7 @@ python $MODULE run_skimage.py external.tifffile.imsave data/skimage/image.tiff t
 python $MODULE run_skimage.py io.load_sift data/skimage/sift.key 
 python $MODULE run_skimage.py io_load_surf data/skimage/image.surf 
 
-mkdir tmp/pandas
+mkdir -p tmp/pandas
 python $MODULE run_pandas.py read_csv data/pandas/dataframe.csv
 python $MODULE run_pandas.py read_table data/pandas/dataframe.csv
 python $MODULE run_pandas.py read_excel data/pandas/dataframe.xls
@@ -65,7 +65,7 @@ python $MODULE run_pandas.py Series.to_hdf tmp/pandas/series.hdf
 python $MODULE run_pandas.py Series.to_msgpack tmp/pandas/series.mpack
 python $MODULE run_pandas.py Series.to_pickle tmp/pandas/series.pickle
 
-mkdir tmp/nibabel
+mkdir -p tmp/nibabel
 python $MODULE run_nibabel.py nifti1.Nifti1Image.from_filename data/nibabel/nifti1_image
 python $MODULE run_nibabel.py nifti1.Nifti1Image.to_filename tmp/nibabel/nifti1_image
 python $MODULE run_nibabel.py nifti2.Nifti2Image.from_filename data/nibabel/nifti2_image
@@ -85,7 +85,7 @@ python $MODULE run_nibabel.py parrec.PARRECImage.to_filename data/nibabel/parrec
 python $MODULE run_nibabel.py spm2analyze.Spm2AnalyzeImage.from_filename data/nibabel/spm2_image
 python $MODULE run_nibabel.py spm2analyze.Spm2AnalyzeImage.to_filename tmp/nibabel/spm2_image
 
-mkdir tmp/gdal
+mkdir -p tmp/gdal
 python $MODULE run_gdal.py Open data/gdal/image.tiff
 python $MODULE run_gdal.py Driver.Create tmp/gdal/image.tiff
 python $MODULE run_gdal.py Driver.CreateCopy data/gdal/image.tiff tmp/gdal/imagecopy.tiff
